70f42090352befe7de9a18acf58878fccf795df3
Fabricio Basilio <fabriciobasilioo@gmail.com>
2025-08-27 10:25:26 -0300

    feat: adição da função de filtrar pela categoria

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './styles/App.css'
 import Todo from './components/Todo'
 import TodoForm from './components/TodoForm'
 import Search from './components/Search'
+import Filter from './components/Filter'
 
 
 function App() {
@@ -29,6 +30,9 @@ function App() {
   ])
 
   const [search, setSearch] = useState("")
+  const [filter, setFilter] = useState("All")
+  const [sort, setSort] = useState("Asc")
+
 
 function addTodo(text, category) {
   const newTodos = [
@@ -64,8 +68,11 @@ function completeTodo(id) {
     <div className="app">
       <h1>Lista de Tarefas</h1>
       <Search search={search} setSearch={setSearch}/>
+      <Filter filter={filter} setFilter={setFilter}/>
       <section className="todo_list">
-        {todos.filter(todo => todo.text.toLowerCase().includes(search.toLowerCase())).map(todo => (
+        {todos
+        .filter(todo => filter === "All" ? true : filter === "Done" ? todo.isDone : !todo.isDone)
+        .filter(todo => todo.text.toLowerCase().includes(search.toLowerCase())).map(todo => (
           <Todo key={todo.id} todo={ todo } completeTodo={ completeTodo } removeTodo={ removeTodo }/>
         ))}
       </section>
---
+++ src/components/Filter.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+function Filter({ filter, setFilter }) {
+  return (
+    <section className="filter">
+        <h2>Filtrar</h2>
+        <div className="filter_options">
+            <div>
+                <p>Status: </p>
+                <select value={filter} onChange={e => setFilter(e.target.value)}>
+                    <option value="All">Todas</option>
+                    <option value="Done">Feitas</option>
+                    <option value="Incomplete">Incompletas</option>
+                </select>
+            </div>
+            <div>
+                <p>Ordem alfabética: </p>
+                <div className="filter_options__alphabetic__buttons">
+                    <button>Asc</button>
+                    <button>Desc</button>
+                </div>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Filter
