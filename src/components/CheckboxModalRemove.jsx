795cf34f130fc561232cd83dfe7cd209603d4ffd
Fabricio Basilio <fabriciobasilioo@gmail.com>
2025-08-30 21:11:40 -0300

    feat: conserto da estilizacao do checkbox quando checado

--- src/components/CheckboxModalRemove.jsx
+++ src/components/CheckboxModalRemove.jsx
@@ -1,8 +1,16 @@
+import { useEffect } from "react";
+
 function CheckboxModalRemove({checkboxModalRemove, setCheckboxModalRemove}) {
+
+  useEffect(() => {
+    console.log(checkboxModalRemove);
+    
+  }, [checkboxModalRemove])
+
   return (
     <section className="checkbox">
-      <input type="checkbox" id="removeTodoCheckbox" onClick={() => setCheckboxModalRemove(!checkboxModalRemove)}/>
-      <span></span>
+      <input type="checkbox" id="removeTodoCheckbox" className={checkboxModalRemove ? "checkbox--ativo" : ""} onClick={() => setCheckboxModalRemove(!checkboxModalRemove)}/>
+      <span className={checkboxModalRemove ? "checkbox--ativo" : ""} onClick={() => setCheckboxModalRemove(!checkboxModalRemove)}></span>
       <label htmlFor="removeTodoCheckbox">Perguntar antes de remover</label>
     </section>
   );
