fd5137efc47397424e41ab3824aeea9bbb351cbb
Fabricio Basilio <fabriciobasilioo@gmail.com>
2025-09-13 16:08:53 -0300

    feat: adicao de estado como array de objetos para categorias

---
+++ src/components/Options.jsx
@@ -0,0 +1,16 @@
+function Options({ categories }) {
+  console.log(categories);
+
+  return (
+    <>
+      <option value="">Selecione uma categoria</option>
+      {categories.map((category) => (
+        <option key={category.id} value={category.categoryName}>
+          {category.categoryName}
+        </option>
+      ))}
+    </>
+  );
+}
+
+export default Options;
--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,8 +1,42 @@
 import { useState } from "react";
+import Options from "./Options";
 
 function TodoForm({ addTodo, setFade, setModalForm }) {
   const [value, setValue] = useState("");
   const [category, setCategory] = useState("");
+
+  const [categories, setCategories] = useState([
+    {
+      id: Math.floor(Math.random() * 100),
+      categoryName: "Pessoal",
+    },
+    {
+      id: Math.floor(Math.random() * 100),
+      categoryName: "Estudos",
+    },
+    {
+      id: Math.floor(Math.random() * 100),
+      categoryName: "Trabalho",
+    },
+    {
+      id: Math.floor(Math.random() * 100),
+      categoryName: "Esporte",
+    },
+  ]);
+
+  // function addCategory(categoryName) {
+  //   const newCategories = [
+  //     ...categories,
+  //     {
+  //       id: Math.floor(Math.random() * 100),
+  //       categoryName: categoryName,
+  //     },
+  //   ];
+
+  //   setCategories(newCategories);
+  // }
+
+  // addCategory("")
 
   function handleFormSubmit(e) {
     e.preventDefault();
@@ -40,11 +74,7 @@ function TodoForm({ addTodo, setFade, setModalForm }) {
           id="categoria_adicionar_tarefa"
           onChange={(e) => setCategory(e.target.value)}
         >
-          <option value="">Selecione uma categoria</option>
-          <option value="Trabalho">Trabalho</option>
-          <option value="Pessoal">Pessoal</option>
-          <option value="Estudos">Estudos</option>
-          <option value="Esporte">Esporte</option>
+          <Options categories={categories} />
         </select>
         <button type="submit">Criar tarefa</button>
       </form>
